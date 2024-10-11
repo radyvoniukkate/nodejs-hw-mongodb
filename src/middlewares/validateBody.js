fe80f8af567ec553a8a692e3bbd71464beff9c62
radyvoniuk <radyvoniukkate@gmail.com>
2024-10-11 11:54:55 +0300

    fixing validation

--- src/middlewares/validateBody.js
+++ src/middlewares/validateBody.js
@@ -1,24 +1,8 @@
 
 const validateBody = (req, res, next) => {
 
-  const { name, email, phoneNumber } = req.body;
-  console.log('Received body:', req.body); 
+  const { email } = req.body;
 
-  if (!name || name.length < 3 || name.length > 20) {
-    return res.status(400).json({
-      status: 400,
-      message: `"name" is required and should be between 3 and 20 characters`,
-      data: null,
-    });
-  }
-
-  if (!phoneNumber || phoneNumber.length < 3 || phoneNumber.length > 20) {
-    return res.status(400).json({
-      status: 400,
-      message: `"phoneNumber" is required and should be between 3 and 20 characters`,
-      data: null,
-    });
-  }
 
   const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
   if (!email || !emailRegex.test(email)) {
@@ -34,6 +18,3 @@ const validateBody = (req, res, next) => {
 
 module.exports = validateBody;
 
-
-
-module.exports = validateBody;
